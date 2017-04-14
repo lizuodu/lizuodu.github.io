b034ceb5ec99a0a9e0a8c7f73c8c4f606328acc8
lizuodu <lizuodu@163.com>
2017-04-14 23:29:16 +0800

    重新调整

---
+++ web/assets/javascripts/app.js
@@ -0,0 +1,49 @@
+;
+$(function() {
+
+    function runCheck() {
+        if($.browser.msie) {
+            var msg = [
+               '<h3>非常抱歉，我的简历当前对IE支持不完整;</h3>'
+            ].join("");
+            $("#notify").setTemplate(msg);
+            $("#notify").processTemplateURL("");
+            return false;
+        }
+        return true;            
+    }
+
+    function render() {
+        var d = new Date();
+        var currYear = d.getFullYear();
+        $("#resume").setTemplateElement("resumetpl");
+        $("#resume").setParam("year", currYear);
+        $("#resume").processTemplateURL("data/resume_cn.json");
+    }
+    
+    function loadPhoto() {
+        var photoUrl = "assets/images/photo.png";
+        var $photo = $(".photo");
+        $("h1").hover(function(){ 
+            $photo.removeClass("out").addClass("fade");
+            $.ajax({
+                url: photoUrl,
+                cache: true
+            }).always(function(){
+                $photo.find("img").attr("src", photoUrl);
+            });
+        }, function() {
+            $photo.removeClass("fade").addClass("out");
+            $photo.find("img").attr("src", null);
+        });
+    }
+
+    (function init() {
+        runCheck() && render();
+        Pace.on("hide", function() {
+            $("#resume").removeClass("out").addClass("fade");
+            //loadPhoto();
+        });
+    })();
+
+});
